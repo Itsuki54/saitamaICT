6bb10279c32ad50430dd81b2528b06331f802d43
RitsuMurakami <beinosuke01@gmail.com>
2024-06-07 17:58:43 +0900

    add: exec button

--- flutter/lib/app.dart
+++ flutter/lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Typist',
+      title: 'KUDAMONO Code',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: const ColorScheme.light(
@@ -46,27 +46,63 @@ class TextEditor extends StatefulWidget {
   const TextEditor({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _TextEditor createState() => _TextEditor();
+  TextEditorState createState() => TextEditorState();
 }
 
-class _TextEditor extends State<TextEditor> {
+class TextEditorState extends State<TextEditor> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Row(children: [
-            Text("filename", style: GoogleFonts.anton(fontSize: 32))
-          ]),
-          leading: BackButton(
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          actions: [],
-          backgroundColor: Colors.black,
-        ),
+        appBar: EditorAppBar(),
         body: Column(
           children: [],
         ));
+  }
+}
+
+class EditorAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const EditorAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      title: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Text("filename", style: GoogleFonts.anton(fontSize: 32)),
+        const Expanded(child: SizedBox()),
+        const Padding(
+            padding: EdgeInsets.all(5), child: ExecButton(isOn: false)),
+      ]),
+      leading: BackButton(
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+      backgroundColor: Colors.black,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(63);
+}
+
+class ExecButton extends StatefulWidget {
+  const ExecButton({super.key, this.isOn = false});
+
+  final bool isOn;
+
+  @override
+  ExecButtonState createState() => ExecButtonState();
+}
+
+class ExecButtonState extends State<ExecButton> {
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 24,
+      child: Icon(
+        Icons.terminal,
+        color: widget.isOn ? Colors.amber : Colors.grey,
+        size: 28,
+      ),
+    );
   }
 }
