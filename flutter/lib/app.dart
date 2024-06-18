425e19cf498647623b6d528ea5269801ddb38704
RitsuMurakami <beinosuke01@gmail.com>
2024-06-18 13:43:47 +0900

    remove: dark theme

--- flutter/lib/app.dart
+++ flutter/lib/app.dart
@@ -12,29 +12,16 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         splashColor: Colors.white,
-        colorScheme: const ColorScheme.light(
+        colorScheme: ColorScheme.light(
           primary: Colors.black, // 主要な色
           secondary: Colors.grey, // セカンダリ色
-          surface: Colors.white, // 表面色
+          surface: Colors.grey.shade900, // 表面色
           error: Colors.red, // エラー色
           onPrimary: Colors.white, // 主要な色の上でのテキスト色
           onSecondary: Colors.black,
           onSurface: Colors.black,
           onError: Colors.red, // エラー色の上でのテキスト色
           brightness: Brightness.light, // 明るさ
-        ),
-        useMaterial3: true,
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.dark(
-          primary: Colors.grey.shade800, // 主要な色
-          secondary: Colors.white, // セカンダリ色
-          surface: const Color(0xFF363636), // 表面色
-          error: Colors.red, // エラー色
-          onPrimary: Colors.white, // 主要な色の上でのテキスト色
-          onSecondary: Colors.white,
-          onSurface: Colors.white,
-          onError: Colors.red, // エラー色の上でのテキスト色
         ),
         useMaterial3: true,
       ),
